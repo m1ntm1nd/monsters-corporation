734f3edb8d108c585e1f1fab34a3b3e89409d080
Pavel Kulagin <kulagin.pavel.unn@gmail.com>
2021-07-15 02:05:33 +0300

    crop face + detect smile

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ def detect_face(image, exec_net, input_blob, out_blob):
 
     input = cv2.resize(image, (INPUT_WIDTH, INPUT_HEIGHT))
     image = cv2.resize(image, (INPUT_WIDTH, INPUT_HEIGHT))
+    res = input
     input = input.transpose(2, 0, 1)
 
     output = exec_net.infer(inputs={input_blob : input})
@@ -46,18 +47,20 @@ def detect_face(image, exec_net, input_blob, out_blob):
             point1, point2 = (int(detection[3]*INPUT_WIDTH), int(detection[4]*INPUT_HEIGHT)), (int(detection[5]*INPUT_WIDTH), int(detection[6]*INPUT_HEIGHT))
             xmin, ymin, xmax, ymax = int(detection[3]*INPUT_WIDTH), int(detection[4]*INPUT_HEIGHT), int(detection[5]*INPUT_WIDTH), int(detection[6]*INPUT_HEIGHT)         
             #cv2.rectangle(image, point1, point2, color, line_width)
-            res = image[ymin:ymax, xmin:xmax]
+            res = res[ymin:ymax, xmin:xmax]
             #cv2.imwrite('images/{}.jpg'.format(flag), res) 
         if flag > 5:
             break
     
-    return res
+    res = cv2.resize(res, (256, 256))
+
+    return res, image
 
 
-def recognize_smile(image, exec_net, input_blob, out_blob):
+def recognize_smile(image, exec_net, input_blob, out_blob, frame):
     input = cv2.resize(image, (64, 64))
     input = input.transpose(2, 0, 1)
-    result = image
+    result = frame
 
     output = exec_net.infer(inputs={input_blob : input})
     if np.argmax(output[out_blob]) == 1:
@@ -97,7 +100,7 @@ def main():
     out_blob = next(iter(net.outputs))
     input_blob = next(iter(net.inputs))
 
-    net2 = ie.read_network(model="/home/fuse-power/github/monsters-corporation/emotions-recognition-retail-0003/FP16-INT8/emotions-recognition-retail-0003.xml")
+    net2 = ie.read_network(model="emotions-recognition-retail-0003/FP16-INT8/emotions-recognition-retail-0003.xml")
     exec_net2 = ie.load_network(network=net2, device_name="CPU")
     out_blob2 = next(iter(net2.outputs))
     input_blob2 = next(iter(net2.inputs))
@@ -106,8 +109,8 @@ def main():
         start_time = perf_counter()
 
         ret, frame = cap.read()
-        frame = detect_face(frame, exec_net, input_blob, out_blob)
-        frame = recognize_smile(frame, exec_net2, input_blob2, out_blob2)
+        face, frame = detect_face(frame, exec_net, input_blob, out_blob)
+        face = recognize_smile(face, exec_net2, input_blob2, out_blob2, frame)
 
         end_time = perf_counter()
         log.info("1 frame consuming {} sec".format(end_time - start_time))
